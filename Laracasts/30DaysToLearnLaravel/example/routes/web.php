0f607577a7fd8671a3ddd0cd98387eb65b0ee893
AonghasMackay <aonghasmackay@yahoo.co.uk>
2024-06-07 14:08:42 +0100

    pass data to jobs page

--- Laracasts/30DaysToLearnLaravel/example/routes/web.php
+++ Laracasts/30DaysToLearnLaravel/example/routes/web.php
@@ -3,11 +3,30 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'title' => 'PHP Developer',
+                'description' => 'We are looking for a PHP developer to join our team.',
+                'salary' => 50000
+            ],
+            [
+                'title' => 'Frontend Developer',
+                'description' => 'We are looking for a Frontend developer to join our team.',
+                'salary' => 60000
+            ],
+            [
+                'title' => 'Backend Developer',
+                'description' => 'We are looking for a Backend developer to join our team.',
+                'salary' => 70000
+            ]
+        ]
+    ]);
 });
 
 Route::get('/contact', function () {
